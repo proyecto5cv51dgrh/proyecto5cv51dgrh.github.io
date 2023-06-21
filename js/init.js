eb03ed6d10a272b1988c1466658fdd733424a21e
chat5cv51dgrh <chat5cv51dgrh@gmail.com>
2023-06-20 21:32:14 -0600

    alfin

---
+++ js/init.js
@@ -0,0 +1,25 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyARoGfdVpqR9D_lAufrDRx8jy9sYqzt5go",
+
+  authDomain: "proyecto5cv51dgrh-3fb99.firebaseapp.com",
+
+  projectId: "proyecto5cv51dgrh-3fb99",
+
+  storageBucket: "proyecto5cv51dgrh-3fb99.appspot.com",
+
+  messagingSenderId: "23308626252",
+
+  appId: "1:23308626252:web:f756c15854e49fdcce07e9",
+
+  measurementId: "G-S2EWHYS568"
+
+});
